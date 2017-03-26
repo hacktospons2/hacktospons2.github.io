7b7a32f63c3d70dc7f2b65a6ad926eab243770b9
Sandeep Sidhu <sandeep1654@gmail.com>
2017-03-26 08:35:28 +0000

    modify tables

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+    var mainText = document.getElementById("mainText");
+    var submitBtn = document.getElementById("submitBtn");
+    
+    function submitClick()
+    {
+        var firebaseRef = firebase.database().ref();
+        var messageText = mainText.value;
+        firebaseRef.push().set(messageText);
+    }
+    
+    vart rootRef = firebase.database().ref().child("Users"];
+    rootRef.on("child_added", snap => {
+    alert(snap.val());
+    });
